11fe64ee722f4442010007aa5e4a8a2d4fc9b68b
Enfau Kerus <kerus@enfa.uk>
2021-11-15 08:00:49 +0000

    add misc image function

--- indoman/namespaces/images.py
+++ indoman/namespaces/images.py
@@ -18,6 +18,25 @@ class Images(Namespace):
         except DockerException as ex:
             return format_error(ex)
 
+    def on_load(self, sid, data):
+        try:
+            images = docker.client.images.load(data)
+            return [image.attrs for image in images]
+        except DockerException as ex:
+            return format_error(ex)
+
+    def on_remove(self, sid, name, force=False, noprune=True):
+        try:
+            image = docker.client.images.remove(image=name, force=force, noprune=noprune)
+        except DockerException as ex:
+            return format_error(ex)
+
+    def on_search(self, sid, term, limit=50):
+        try:
+            return docker.client.images.search(term=term, limit=limit)
+        except DockerException as ex:
+            return format_error(ex)
+
     def on_list(self, sid, repository=None, all=False, filters=None):
         try:
             image = docker.client.images.list(repository, all, filters)
