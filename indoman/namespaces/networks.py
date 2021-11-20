2071adf92e3587232a66fbfd9cb310d44941b00b
Enfau Kerus <kerus@enfa.uk>
2021-11-20 16:56:12 +0000

    add misc network functions

--- indoman/namespaces/networks.py
+++ indoman/namespaces/networks.py
@@ -1,4 +1,6 @@
 from docker.errors import DockerException
+from docker.models.containers import Container
+from docker.models.networks import Network
 from socketio import Namespace
 
 from indoman.utils import docker, logging, format_error
@@ -8,5 +10,34 @@ class Networks(Namespace):
         try:
             networks = docker.client.networks.list(**list_params)
             return [c.attrs for c in networks]
+        except DockerException as ex:
+            return format_error(ex)
+
+    def on_create(self, sid, name, create_params={}):
+        try:
+            docker.client.networks.create(name, **create_params)
+        except DockerException as ex:
+            return format_error(ex)
+
+    def on_remove(self, sid, network_id):
+        try:
+            network: Network = docker.client.networks.get(network_id)
+            network.remove()
+        except DockerException as ex:
+            return format_error(ex)
+
+    def on_connect(self, sid, container_id, network_id, connect_params={}):
+        try:
+            network: Network = docker.client.networks.get(network_id)
+            container: Container = docker.client.containers.get(container_id)
+            network.connect(container, **connect_params)
+        except DockerException as ex:
+            return format_error(ex)
+
+    def on_disonnect(self, sid, container_id, network_id):
+        try:
+            network: Network = docker.client.networks.get(network_id)
+            container: Container = docker.client.containers.get(container_id)
+            network.disconnect(container)
         except DockerException as ex:
             return format_error(ex)
